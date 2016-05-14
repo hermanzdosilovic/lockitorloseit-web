0959d57c520f5aa5bb0a56b9f9c705718768a51d
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-05-14 16:59:37 +0200

    Basic structure

--- Gemfile
+++ Gemfile
@@ -6,18 +6,14 @@ gem 'pg', '~> 0.15'
 
 gem 'devise', '~> 4.1'
 gem 'dotenv-rails', '~> 2.1'
-gem 'jquery-rails'
 gem 'mina', '~> 0.3.8'
 gem 'sass-rails', '~> 5.0'
 gem 'slim-rails', '~> 3.0'
-gem 'turbolinks'
-gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
-  gem 'byebug'
+  gem 'byebug', '~> 9.0'
 end
 
 group :development do
   gem 'web-console', '~> 2.0'
-  gem 'spring'
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,38 @@
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery-1.12.3.min
+//= require bootstrap.min
+//= require custom
 //= require_tree .
+
+$(document).ready(function() {
+  changeMinHeightOnResize(".greeting", 0);
+  changeMinHeightOnResize(".lock-it-wallet", 0);
+  changeMinHeightOnResize(".lock-it-ring", 0);
+
+  var $window = $(window);
+  var $navbar = $(".navbar-default");
+
+  $("a.scroll").click(function() {
+    $('.navbar-collapse').collapse('hide');
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+  });
+
+  if ($window.width() <= 767) {
+    $navbar.addClass("navbar-fixed-top");
+  } else {
+    $navbar.removeClass("navbar-fixed-top");
+  }
+  $window.resize(function() {
+    if ($window.width() <= 767) {
+      $navbar.addClass("navbar-fixed-top");
+    } else {
+      $navbar.removeClass("navbar-fixed-top");
+    }
+  });
+
+  $('body').scrollspy({
+    target: '.navbar-default'
+  });
+});
