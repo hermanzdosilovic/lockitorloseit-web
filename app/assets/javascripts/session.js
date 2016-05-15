1d566164a82a8a1f19301f475317c0a7c83659ca
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-05-15 16:16:58 +0200

    Changed sessions layout

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -1,4 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
+//= require custom
 //= require_tree .
+
+$(document).ready(function() {
+  changeMinHeightOnResize(".sessions-form", 0);
+});
